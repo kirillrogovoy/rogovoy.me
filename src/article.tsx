c54669d92c5e3cac9d73d55e4ba947c599820a2c
Kirill Rogovoy <kirill@rogovoy.me>
2020-02-11 15:26:33 +0200

    Update

--- src/article.tsx
+++ src/article.tsx
@@ -26,7 +26,7 @@ const articles: ArticleMeta[] = [
     title: 'Here are some amazing advantages of go that you dont hear much about',
     description: 'test description',
     tags: ['test'],
-    hidden: false,
+    hidden: true,
     tweetId: '1162459837910978561',
   },
 ]
