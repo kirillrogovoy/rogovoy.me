4ad6ed66fae8c03f5d6cfffcc2de7106ad8b7009
Kirill Rogovoy <kirill@rogovoy.me>
2020-03-06 18:28:31 +0200

    Publish

---
+++ out/_next/static/webpack/styles.d468a2b48d34175881df.hot-update.js
@@ -0,0 +1,41 @@
+webpackHotUpdate("styles",{
+
+/***/ "./src/tailwind.css":
+/*!**************************!*\
+  !*** ./src/tailwind.css ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin;
+    if (true) {
+      var injectCss = function injectCss(prev, href) {
+        var link = prev.cloneNode();
+        link.href = href;
+        link.onload = function() {
+          prev.parentNode.removeChild(prev);
+        };
+        prev.stale = true;
+        prev.parentNode.insertBefore(link, prev);
+      };
+      module.hot.dispose(function() {
+        window.__webpack_reload_css__ = true;
+      });
+      if (window.__webpack_reload_css__) {
+        module.hot.__webpack_reload_css__ = false;
+        console.log("[HMR] Reloading stylesheets...");
+        var prefix = document.location.protocol + '//' + document.location.host;
+        document
+          .querySelectorAll("link[href][rel=stylesheet]")
+          .forEach(function(link) {
+            if (!link.href.match(prefix) || link.stale) return;
+            injectCss(link, link.href.split("?")[0] + "?unix=1583511652519");
+          });
+      }
+    }
+  
+
+/***/ })
+
+})
+//# sourceMappingURL=styles.d468a2b48d34175881df.hot-update.js.map
